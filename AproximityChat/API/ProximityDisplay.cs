9bcd27227721af1731c74c68ff815f4fb1b8e42d
Unbistrackted <112902220+Unbistrackted@users.noreply.github.com>
2025-02-01 15:12:08 -0300

    Arrumar lógica em ProximityDisplay.cs e adicionar novos Chats

--- AproximityChat/API/ProximityDisplay.cs
+++ AproximityChat/API/ProximityDisplay.cs
@@ -1,8 +1,10 @@
-﻿namespace AproximityChat.API;
+namespace AproximityChat.API;
 
-using BikeUtils;
 using Exiled.API.Enums;
 using Exiled.API.Features;
+using InventorySystem;
+using InventorySystem.Items;
+using InventorySystem.Items.Radio;
 using InventorySystem.Items.Usables.Scp1576;
 using PlayerRoles;
 using System.Linq;
@@ -10,6 +12,7 @@ using UnityEngine;
 
 public class ProximityChatLogic
 {
+    public static Player? RRreceiver;
     public static string[] BaseMsgBrod = new string[]
     {
         "<size=30><color=#999999>[Prox.C]</color></size>",
@@ -20,102 +23,109 @@ public class ProximityChatLogic
         "<size=30><color=#940000>[Scp1576.C]</color></size>",
     };
 
-    public static void ProximityChat(Player player, string message)
+    public static void ProximityChat(Player Sender, string message, Player? _sender = null)
     {
-        //scp1576
-        if (player.CurrentItem != null && player.CurrentItem.Type == ItemType.SCP1576)
-        {
-            if (player.CurrentItem.Base is Scp1576Item scp1576)
-            {
-                if (scp1576.IsUsing)
-                {
-                    SpectatorChat(player, message);
-                }
-            }
-        }
-
+        Log.Info(RRreceiver);
         //normal
-        foreach (Player ply in Player.List.Where(p => p.Role.Type != RoleTypeId.Scp939 && Vector3.Distance(p.Position, player.Position) <= AProximityChat.Instance.Config.distancia))
+        foreach (Player Receiver in Player.List.Where(p => p.Role.Type != RoleTypeId.Scp939 && Vector3.Distance(p.Position, Sender.Position) <= AProximityChat.Instance.Config.distancia && p != RRreceiver && p !=_sender))
         {
-            ply.Broadcast(3, $"{BaseMsgBrod[0]} <color={RoleClassUtils.GetRoleColor(player.Role.Type)}>({player.Nickname})</color>\n{message}");
-            SpecChatProx(ply, message);
+            Receiver.Broadcast(3, $"{BaseMsgBrod[0]} <color={RoleClassUtils.GetRoleColor(Sender.Role.Type)}>({Sender.Nickname})</color>\n{message}");
+            SpectatorSeeProximityChat(Receiver, message);
         }
+        RRreceiver = null;
 
-        //939 ouvidão
-        foreach (Player ply in Player.List.Where(p => p.Role.Type == RoleTypeId.Scp939 && Vector3.Distance(p.Position, player.Position) <= AProximityChat.Instance.Config.distancia + 2))
+        //939 ouve a maior distancia lmao
+        foreach (Player Receiver in Player.List.Where(p => p.Role.Type == RoleTypeId.Scp939 && Vector3.Distance(p.Position, Sender.Position) <= AProximityChat.Instance.Config.distancia + 2))
         {
-            if (Vector3.Distance(ply.Position, player.Position) < 7)
+            if (Vector3.Distance(Receiver.Position, Sender.Position) < 7)
             {
-                ply.Broadcast(3, $"<size=25><color=#999999>[perto]</color></size>{BaseMsgBrod[0]} <color={RoleClassUtils.GetRoleColor(player.Role.Type)}>({player.Nickname})</color>\n{message}");
+                Receiver.Broadcast(3, $"<size=25><color=#999999>[perto]</color></size>{BaseMsgBrod[0]} <color={RoleClassUtils.GetRoleColor(Sender.Role.Type)}>({Sender.Nickname})</color>\n{message}");
             }
             else
             {
-                ply.Broadcast(3, $"<size=25><color=#999999>[longe]</color></size>{BaseMsgBrod[0]} <color={RoleClassUtils.GetRoleColor(player.Role.Type)}>({player.Nickname})</color>\n{message}");
+                Receiver.Broadcast(3, $"<size=25><color=#999999>[longe]</color></size>{BaseMsgBrod[0]} <color={RoleClassUtils.GetRoleColor(Sender.Role.Type)}>({Sender.Nickname})</color>\n{message}");
             }
         }
     }
 
     public static void ScpChat(Player player, string message)
     {
-        foreach (Player ply in Player.List.Where(p => p.Role.Side == Side.Scp))
+        foreach (Player Receiver in Player.List.Where(p => p.Role.Side == Side.Scp))
         {
-            ply.Broadcast(3, $"{BaseMsgBrod[1]} <color=red>({RoleClassUtils.TranslateRole(player.Role.Type)})</color><color=#940000><size=25>({player.Nickname})</size></color>\n<color=#ffb5b5>{message}</color>");
+            Receiver.Broadcast(3, $"{BaseMsgBrod[1]} <color=red>({RoleClassUtils.TranslateRole(player.Role.Type)})</color><color=#940000><size=25>({player.Nickname})</size></color>\n<color=#ffb5b5>{message}</color>");
         }
     }
 
-    public static void RadioChat(Player player, string message)
+    public static bool RadioChat(Player Sender, string message, out Player? radioWorked)
     {
-        Log.Info($"Radio Chat {message}");
+        RadioItem _radioS;
+        RadioItem _radioR;
+        if (Sender.Inventory.TryGetInventoryItem(ItemType.Radio, out ItemBase radioS))
+        {
+            _radioS = (RadioItem)radioS;
+            if (!_radioS.IsUsable)
+            {
+                radioWorked = null;
+                return false;
+
+            }
+        }
+        else
+        {
+           radioWorked = null;
+           return false;
+        }
+        foreach (Player Receiver in Player.List.Where(p => p != Sender && p.HasItem(ItemType.Radio)))
+        {
+            Receiver.Inventory.TryGetInventoryItem(ItemType.Radio, out ItemBase radioR);
+            _radioR = (RadioItem)radioR;
+            if (_radioR.IsUsable && _radioS.RangeLevel >= _radioR.RangeLevel)
+            {
+                Receiver.Broadcast(3, $"{BaseMsgBrod[2]}-{_radioS.RangeLevel} ({Sender.Nickname})\n{message}");
+                RRreceiver = Receiver;
+            }
+        }
+        Sender.Broadcast(3, $"{BaseMsgBrod[2]}-{_radioS.RangeLevel} ({Sender.Nickname})\n{message}");
+        radioWorked = Sender;
+        return true;
     }
 
     public static void SpectatorChat(Player player, string message)
     {
-        //spectador
-        foreach (Player ply in Player.List.Where(p => p.Role.Type == RoleTypeId.Spectator))
+        foreach (Player Receiver in Player.List.Where(p => p.Role.Type == RoleTypeId.Spectator))
         {
-            if (player.Role.Type != RoleTypeId.Spectator)
+             Receiver.Broadcast(3, $"{BaseMsgBrod[3]} ({player.Nickname})\n{message}");
+        }
+    }
+    public static bool Scp1576Chat(Player player, string message)
+    {
+        foreach (Player Sender in Player.List.Where(p => p.CurrentItem != null && p.CurrentItem.Type == ItemType.SCP1576))
+        {
+            if (Sender.CurrentItem.Base is Scp1576Item scp1576)
             {
-                ply.Broadcast(3, $"{BaseMsgBrod[5]} ({player.Nickname})\n{message}");
+                if (scp1576.IsUsing)
+                {
+                    Sender.Broadcast(3, $"{BaseMsgBrod[5]} ({player.Nickname})\n{message}");
+                    SpectatorChat(player, message);
+                }
             }
             else
             {
-                ply.Broadcast(3, $"{BaseMsgBrod[3]} ({player.Nickname})\n{message}");
+                return false;
             }
         }
-
-        //pra quem esta usando o item scp1576
-        foreach (Player ply in Player.List.Where(p => p.CurrentItem != null && p.CurrentItem.Type == ItemType.SCP1576 && p != player))
-        {
-            if (ply.CurrentItem.Base is Scp1576Item scp1576)
-            {
-                if (scp1576.IsUsing)
-                {
-                    ply.Broadcast(3, $"{BaseMsgBrod[5]} ({player.Nickname})\n{message}");
-                }
-            }
-        }
+        return true;
     }
 
-    public static void SpecChatProx(Player player, string message)
+    public static void SpectatorSeeProximityChat(Player player, string message)
     {
-        if (player.CurrentItem != null)
+        foreach (Player Receiver in Player.List.Where(p => p.IsDead))
         {
-            if (player.CurrentItem.Base is Scp1576Item scp1576)
-            {
-                if (scp1576.IsUsing)
-                {
-                    return;
-                }
-            }
-        }
-
-        foreach (Player ply in Player.List.Where(p => p.IsDead))
-        {
-            if (ply.Role is Exiled.API.Features.Roles.SpectatorRole spect)
+            if (Receiver.Role is Exiled.API.Features.Roles.SpectatorRole spect)
             {
                 if (spect.SpectatedPlayer != null && spect.SpectatedPlayer == player)
                 {
-                    ply.Broadcast(3, $"{BaseMsgBrod[0]} <color={RoleClassUtils.GetRoleColor(player.Role.Type)}>({player.Nickname})</color>\n{message}");
+                    Receiver.Broadcast(3, $"{BaseMsgBrod[0]} <color={RoleClassUtils.GetRoleColor(player.Role.Type)}>({player.Nickname})</color>\n{message}");
                 }
             }
         }
@@ -123,9 +133,9 @@ public class ProximityChatLogic
 
     public static void LobbyChat(Player player, string message)
     {
-        foreach (Player ply in Player.List)
+        foreach (Player Receiver in Player.List)
         {
-            ply.Broadcast(3, $"{BaseMsgBrod[4]} ({player.Nickname})\n{message}");
+            Receiver.Broadcast(3, $"{BaseMsgBrod[4]} ({player.Nickname})\n{message}");
         }
     }
 }
